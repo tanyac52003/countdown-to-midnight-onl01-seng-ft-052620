8dc98efad414d87f23bf8decb5f2087168dd11dd
Tanya Hodges <tanyac52003@gmail.com>
2020-05-27 19:40:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,12 +5,15 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
     number -= 1
   end
-  # puts "HAPPY NEW YEAR!"
   "HAPPY NEW YEAR!"
 end
 
+
 def countdown_with_sleep(number)
   while number > 0 
-  
-
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+    sleep 5
+  end 
+  "HAPPY NEW YEAR!"
 end 
